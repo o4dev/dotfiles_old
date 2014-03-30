3b0934698332a5a7aa539c9fab9fc1589c1d1b24
Luke Southam <luke@devthe.com>
2014-03-31 00:46:54 +0100

    Basic setup for configs.

---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# Sets a new system up with correct configs and dependencies.
+#
+#
+
+REPOURL=
+DOTFILES=$HOME/.dotfiles
+
+set -e
+
+echo 'Installing dependencies...'
+# Assume debian based and install dependencies
+sudo apt-get install zsh vim i3 xfce4-terminal mpv conky nitrogen tmux xfonts-terminus
+
+# Clone oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+
+echo 'Downloading configs....'
+# Assuming script is not being run inside repo, clone repo.
+git clone $REPOURL $DOTFILES
+
+
+
+echo 'Setting up configs...'
+# Simple backup function.
+# Just moves existing configs to ~/.dotfiles.orig
+backup(){
+    if [ -f $HOME/$1 ]; then
+        echo "Backing up $HOME/$1 to $DOTFILES.orig/$1 ...."
+        mkdir -p $DOTFILES.orig
+        mv $HOME/$1 $DOTFILES.orig
+    fi
+}
+
+
+# Create a symbolic link for the standard configs
+configs=(.zshrc .vimrc .conkyrc .tmux.conf .i3status.conf .ncmpcpp .mpdconf .i3)
+for config in $configs; do
+    backup $config
+    ln -s $DOTFILES/$config $HOME/$config
+done
+
+
+# Add modded oh-my-zsh theme
+ln -s $DOTFILES/af-magic-mod.zsh-theme $HOME/.oh-my-zsh/themes
+
+
+# Setup vim plugins etc
+backup .vim
+cp -r $DOTFILES/.vim $HOME/.vim
+
+
+# GTK Setup
+
+# link the theme
+mkdir -p $HOME/.themes
+backup .themes/OMG-Dark
+ln -s $DOTFILES/.themes/OMG-Dark $HOME/.themes
+
+# link gtk 2 config
+backup .gtk-2.0
+ln -s $DOTFILE/.gtkrc-2.0 $HOME
+
+# link gtk 3 config
+mkdir -p $HOME/.config/gtk-3.0
+backup .config/gtk-3.0/settings.ini
+ln -s $DOTFILES/gtk-3.0-settings.ini $HOME/.config/gtk-3.0/settings.ini
+
+
